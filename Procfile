682c44f9b22bf014d6d060e9e535bed746a40e96
EdsonSousa1974 <esousa74@gmail.com>
2024-05-02 20:09:19 -0300

    Aula 45: Colocando o site no ar - Railway + Github

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn edfli.wsgi --log-file -
--- edflix/settings.py
+++ edflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w6zt6kt#=_)8&gj3rl3pl0))6lfjkq%-8&_pgsq0d739=jybpf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,7 +86,16 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)        
+    }
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 AUTH_USER_MODEL = "filme.Usuario"
@@ -121,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
