72cf0dd954a15f322515264abb6cec331ea0d4d4
EdsonSousa1974 <esousa74@gmail.com>
2024-05-02 20:42:26 -0300

    DEBUG = False

--- edflix/settings.py
+++ edflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w6zt6kt#=_)8&gj3rl3pl0))6lfjkq%-8&_pgsq0d739=jybpf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
