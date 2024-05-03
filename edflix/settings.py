c37f68bc3879f0517393b6511295cfe97cbdac5a
EdsonSousa1974 <esousa74@gmail.com>
2024-05-02 21:09:06 -0300

    Atualizando BD URL

--- edflix/settings.py
+++ edflix/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv('postgresql://postgres:eJgzAaHZbeoNgHPmxsuirGPhZHeWSeNk@roundhouse.proxy.rlwy.net:12501/railway')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
