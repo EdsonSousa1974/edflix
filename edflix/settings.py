85a5e487bf5b0e3e08ad827c600885e575593466
EdsonSousa1974 <esousa74@gmail.com>
2024-05-06 21:49:21 -0300

    Corrigindo ulr

--- edflix/settings.py
+++ edflix/settings.py
@@ -89,12 +89,14 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv('postgresql://postgres:eJgzAaHZbeoNgHPmxsuirGPhZHeWSeNk@roundhouse.proxy.rlwy.net:12501/railway')
+DATABASE_URL = os.getenv('postgresql://postgres:mSiKqgjjtJLVLENpBSzHWudZWIcDSUwo@roundhouse.proxy.rlwy.net:33396/railway')
 
+"""
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)        
     }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
