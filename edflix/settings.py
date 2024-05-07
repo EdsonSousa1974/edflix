82732f96bdaceeaec9c3be3e1a0914a5663916af
EdsonSousa1974 <esousa74@gmail.com>
2024-05-06 21:56:40 -0300

    Aula 45 - Deploy

--- edflix/settings.py
+++ edflix/settings.py
@@ -91,12 +91,10 @@ import os
 
 DATABASE_URL = os.getenv('postgresql://postgres:mSiKqgjjtJLVLENpBSzHWudZWIcDSUwo@roundhouse.proxy.rlwy.net:33396/railway')
 
-"""
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)        
     }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
