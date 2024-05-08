8fb86ce108bae17f5476591acd20b9db639bdf3d
EdsonSousa1974 <esousa74@gmail.com>
2024-05-08 20:39:42 -0300

    Update email superuser

--- filme/apps.py
+++ filme/apps.py
@@ -5,7 +5,6 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-"""
 def ready(self):
     from .models import Usuario
     import os
@@ -13,10 +12,6 @@ def ready(self):
     email_os = os.getenv("EMAIL_ADMIN")
     senha_os = os.getenv("SENHA_ADMIN")
     
-    #if not Usuario.objects.filter(email=email_os).exists():
-    usuarios = Usuario.objects.filter(email=email_os)
-    
-    if not usuarios:
+    if not Usuario.objects.filter(email=email_os).exists():
         Usuario.objects.create_superuser(username="admin", email=email_os, password=senha_os,
                                          is_active=True, is_staff=True)
-"""    
