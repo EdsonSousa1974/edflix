96a4aa777ae31da0f7951df92b08c5112e594e83
EdsonSousa1974 <esousa74@gmail.com>
2024-05-10 10:18:41 -0300

    add whitenoise

--- edflix/settings.py
+++ edflix/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'filme',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -145,6 +146,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",   
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL ='media/'
 
 MEDIA_ROOT = BASE_DIR / "media"
@@ -152,8 +155,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+DEFAULT_AUTO_FIELD 
 LOGIN_REDIRECT_URL = 'filme:homefilmes'
 
 LOGIN_URL = 'filme:login'
