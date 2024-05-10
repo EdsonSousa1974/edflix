47533de4c0436f171b2febcc4dd083160b69ff42
EdsonSousa1974 <esousa74@gmail.com>
2024-05-10 19:13:54 -0300

    DATABASE_URL

--- edflix/settings.py
+++ edflix/settings.py
@@ -96,11 +96,11 @@ DATABASES = {
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
-    DATABASES = {
-        'default': {
-            dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)        
-        }
-    }
+        DATABASES = {
+            'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=1800)
+        }  
 #"""
 
 # Password validation
