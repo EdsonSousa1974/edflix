b4d043cd3151024fdc909f3e7b54cc5b03fb7144
EdsonSousa1974 <esousa74@gmail.com>
2024-05-10 19:17:59 -0300

    DATABASE_URL : postgres

--- edflix/settings.py
+++ edflix/settings.py
@@ -98,7 +98,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
         DATABASES = {
             'default': dj_database_url.config(
-            default=DATABASE_URL,
+            default='postgres://postgres:cFBjQKhdjZJCdDqUSpKMAmOOaneTtXUm@monorail.proxy.rlwy.net:44915/railway',
             conn_max_age=1800)
         }  
 #"""
