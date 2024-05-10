daf048227bb7188867eaa7f2b920620f1e4871fa
EdsonSousa1974 <esousa74@gmail.com>
2024-05-10 10:29:05 -0300

    STATICFILES_STORAGE

--- edflix/settings.py
+++ edflix/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'filme',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -146,7 +145,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",   
 ]
 
-# aki STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL ='media/'
 
