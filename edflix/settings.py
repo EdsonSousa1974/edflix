eb755333be322ba04d0fbd6b796c7646c27823d3
EdsonSousa1974 <esousa74@gmail.com>
2024-05-13 16:18:59 -0300

    Sugestão #1

--- edflix/settings.py
+++ edflix/settings.py
@@ -82,8 +82,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'edflix.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# Database # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
