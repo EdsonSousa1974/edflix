950ba6bf35b32892498fe2419558ad992444575c
EdsonSousa1974 <esousa74@gmail.com>
2024-05-13 18:48:40 -0300

    Update DATABASE_URL

--- edflix/settings.py
+++ edflix/settings.py
@@ -92,9 +92,9 @@ DATABASES = {
 }
 
 #"""
-#DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 #DATABASE_URL = 'postgres://...' 
-DATABASE_URL = 'postgres://postgres:xpMMdJflUICWmSqcIfFmIilzAWMgKojQ@monorail.proxy.rlwy.net:25148/railway'
+#DATABASE_URL = 'postgres://postgres:xpMMdJflUICWmSqcIfFmIilzAWMgKojQ@monorail.proxy.rlwy.net:25148/railway'
 
 if DATABASE_URL:    
     DATABASES = {
