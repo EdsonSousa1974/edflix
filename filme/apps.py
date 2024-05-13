229a61f4c50f2dcf9efc870f700a830661067759
EdsonSousa1974 <esousa74@gmail.com>
2024-05-13 18:40:54 -0300

    Criar o usuário se não existir

--- filme/apps.py
+++ filme/apps.py
@@ -11,7 +11,7 @@ class FilmeConfig(AppConfig):
         email = os.getenv("EMAIL_ADMIN")
         senha = os.getenv("SENHA_ADMIN")
         
-        usuarios = Usuario.objects.filter(username=email)    
+        usuarios = Usuario.objects.filter(email=email)    
         if not usuarios:
             Usuario.objects.create_superuser(username="admin", email=email, password=senha,
                                              is_active=True, is_staff=True)
