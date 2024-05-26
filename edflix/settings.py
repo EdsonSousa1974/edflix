2080a33cc4ffda7707f51c84375bb36b0a2370c7
EdsonSousa1974 <esousa74@gmail.com>
2024-05-26 18:41:54 -0300

    Teste Luan

--- edflix/settings.py
+++ edflix/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     }
 }
 
-DATABASE_URL =  env.dj_db_url("DATABASE_URL")
-#DATABASE_URL = 'postgresql://postgres:xpMMdJflUICWmSqcIfFmIilzAWMgKojQ@monorail.proxy.rlwy.net:25148/railway'
+#DATABASE_URL =  env.dj_db_url("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:xpMMdJflUICWmSqcIfFmIilzAWMgKojQ@monorail.proxy.rlwy.net:25148/railway'
 
 #""""
 if DATABASE_URL:    
