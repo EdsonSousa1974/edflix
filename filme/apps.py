3557dda1ed083f668cc43c63c834162f7f451417
EdsonSousa1974 <esousa74@gmail.com>
2024-06-12 20:34:25 -0300

    New admin

--- filme/apps.py
+++ filme/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
-"""
+#"""
     def ready(self):
         from .models import Usuario
         import os
