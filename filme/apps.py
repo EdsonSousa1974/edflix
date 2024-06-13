e42bebc27016cdd5f63421bad2ba2f93e293e10a
EdsonSousa1974 <esousa74@gmail.com>
2024-06-13 06:45:57 -0300

    username="admin"

--- filme/apps.py
+++ filme/apps.py
@@ -11,7 +11,7 @@ class FilmeConfig(AppConfig):
         email_os = os.getenv("EMAIL_ADMIN")
         senha_os = os.getenv("SENHA_ADMIN")
         
-        usuarios = Usuario.objects.filter(email=email_os)    
+        usuarios = Usuario.objects.filter(username="admin")    
         if not usuarios:
             Usuario.objects.create_superuser(username="admin", email=email_os, password=senha_os,
                                              is_active=True, is_staff=True)
