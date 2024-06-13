687cbadab69788451f84e6b82f621ad594c2a8ba
EdsonSousa1974 <esousa74@gmail.com>
2024-06-13 07:02:04 -0300

    20240613 - 07:01

--- filme/apps.py
+++ filme/apps.py
@@ -7,7 +7,7 @@ env.read_env()
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
-#"""
+"""
     def ready(self):
         from .models import Usuario
         import os
